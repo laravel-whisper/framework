a0295f1f163d8ac4dfc5550ca06409673bb8815b
Miguel Piedrafita <github@miguelpiedrafita.com>
2018-09-03 18:13:15 +0200

    Also fix args for all method

--- src/Whisperer.php
+++ src/Whisperer.php
@@ -511,7 +511,7 @@ class Whisperer implements ArrayAccess, Arrayable, Jsonable, JsonSerializable
 
     public static function all()
     {
-        return (new self)->newCollection(collect(static::$client::all(func_get_args()))->map(function ($result) {
+        return (new self)->newCollection(collect(call_user_func_array([static::$client, 'all'], func_get_args()))->map(function ($result) {
             return (new static)->forceFill($result);
         })->toArray());
     }
